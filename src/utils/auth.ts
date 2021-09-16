9f28d3e2f06e17e05c2bfc586ffddcfa36a7019d
netoudi <netoudi@rocketmail.com>
2021-09-16 17:21:21 -0300

    Fix env

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,5 @@
 export const KEYCLOAK_PUBLIC_CONFIG = {
-  realm: String(process.env.NEXT_PUPLIC_KEYCLOAK_REALM),
-  url: String(process.env.NEXT_PUPLIC_KEYCLOAK_URL),
-  clientId: String(process.env.NEXT_PUPLIC_KEYCLOAK_CLIENT_ID),
+  realm: String(process.env.NEXT_PUBLIC_KEYCLOAK_REALM),
+  url: String(process.env.NEXT_PUBLIC_KEYCLOAK_URL),
+  clientId: String(process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID),
 };
