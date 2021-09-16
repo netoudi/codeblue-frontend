e392ac78002da769e2b0f0299f2d0a626bb67ff3
netoudi <netoudi@rocketmail.com>
2021-09-16 16:19:57 -0300

    Config keycloak with cookie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 
-import type { AppProps } from 'next/app';
+import type { AppContext, AppProps } from 'next/app';
 
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
+import { SSRCookies, SSRKeycloakProvider } from '@react-keycloak/ssr';
+
+import { KEYCLOAK_PUBLIC_CONFIG } from 'utils/auth';
+import { parseCookies } from 'utils/cookies';
 
 import theme from 'styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, cookies }: AppProps & { cookies: any }) {
   React.useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     jssStyles?.parentElement?.removeChild(jssStyles);
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SSRKeycloakProvider
+      keycloakConfig={KEYCLOAK_PUBLIC_CONFIG}
+      persistor={SSRCookies(cookies)}
+    >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SSRKeycloakProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  return {
+    cookies: parseCookies(appContext.ctx.req),
+  };
+};
 
 export default MyApp;
