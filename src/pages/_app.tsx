bf03b660862f8a9d0a84bec0bfe1ac7d19b0d13d
netoudi <netoudi@rocketmail.com>
2021-09-17 08:22:00 -0300

    Config silent check sso

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,13 @@ function MyApp({ Component, pageProps, cookies }: AppProps & { cookies: any }) {
     <SSRKeycloakProvider
       keycloakConfig={KEYCLOAK_PUBLIC_CONFIG}
       persistor={SSRCookies(cookies)}
+      initOptions={{
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          typeof window !== 'undefined'
+            ? `${window.location.origin}/silent-check-sso.html`
+            : null,
+      }}
     >
       <ThemeProvider theme={theme}>
         <CssBaseline />
